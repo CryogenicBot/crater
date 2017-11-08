42d6fb034064389bcf4a8f21ee00045687821f79
CryogenicBot <vikram.sharma.vik@gmail.com>
2017-11-07 22:14:37 -0500

    Basic pie chart

--- src/components/stats-column.tsx
+++ src/components/stats-column.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
+import { VictoryPie } from 'victory';
 
 class StatsColumn extends React.Component {
   render() {
     return (
-      <div>
-        <p>something</p>
-      </div>
+      <VictoryPie />
     );
   }
 }
